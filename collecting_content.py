04635a590bb9834f4ab95c59edfe5700f06155b6
juneshone <shamrova.julia@mail.ru>
2024-09-08 21:12:15 +0300

    Add telegram bot

--- collecting_content.py
+++ collecting_content.py
@@ -1,29 +1,18 @@
 import os 
 import re
-import json
 
 
-def collect_content(file_path):
-    quiz_content = {}
-    with open(file_path, 'r', encoding='KOI8-R') as file:
-        text_parts = file.read().split('\n\n')
-        for text_part in text_parts:
-            if 'Вопрос' in text_part:
-                question = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
-            if 'Ответ' in text_part:
-                answer = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
-                quiz_content[question] = answer
-    return quiz_content
-
-
-def main():
+def get_content():
     path = 'quiz-questions'
     for file in os.listdir(path):
         file_path = f'{path}/{file}'
-        quiz_content = collect_content(file_path)
-        with open("quiz_content.json", "w", encoding='KOI8-R') as file:
-            json.dump(quiz_content, file, indent=4, ensure_ascii=False)
-
-
-if __name__ == '__main__':
-    main()
+        quiz_content = {}
+        with open(file_path, 'r', encoding='KOI8-R') as file:
+            text_parts = file.read().split('\n\n')
+            for text_part in text_parts:
+                if 'Вопрос' in text_part:
+                    question = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
+                if 'Ответ' in text_part:
+                    answer = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
+                    quiz_content[question] = answer
+        return quiz_content
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,58 +1,169 @@
 import logging
+import random
+import redis
+import re
 
+from functools import partial
+from enum import Enum
 from environs import Env
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+
+from collecting_content import get_content
 
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 logger = logging.getLogger('telegram_bot')
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.message.from_user.full_name
-    button_list = [
-        [
-            InlineKeyboardButton("Новый вопрос", callback_data='1'),
-            InlineKeyboardButton("Сдаться", callback_data='2'),
-        ],
-        [InlineKeyboardButton("Мой счёт", callback_data='3')],
+class States(Enum):
+    CHOOSING = 1
+    SCORE = 2
+
+
+def start(update, _):
+    user = update.message.from_user.first_name
+    reply_keyboard = [
+        ['Новый вопрос', 'Сдаться'],
+        ['Мой счёт']
     ]
-    reply_markup = InlineKeyboardMarkup(button_list)
+    markup_key = ReplyKeyboardMarkup(
+        reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+    )
+
     update.message.reply_text(
-        f'Привет {user}! Я бот для викторины!',
-        reply_markup=reply_markup,
+        f'Привет {user}! Я бот для викторины!\n\n'
+        'Команда /cancel, чтобы прекратить разговор.\n\n',
+        reply_markup=markup_key, )
+    return States.CHOOSING
+
+
+def handle_new_question_request(update, _, redis_db):
+    user = update.message.from_user
+    logger.info('%s: %s', user.first_name, update.message.text)
+    quiz_content = get_content()
+    question, answer = random.choice(list(quiz_content.items()))
+    redis_db.set(user.id, answer)
+
+    update.message.reply_text(
+        text=question,
+    )
+    return States.SCORE
+
+
+def handle_solution_attempt(update, _, redis_db):
+    user = update.message.from_user
+    logger.info('%s: %s', user.first_name, update.message.text)
+    answer = re.sub(r"[\(\[].*?[\)\]]", "", redis_db.get(user.id)).strip()[:-1]
+    if update.message.text == answer:
+        reply_keyboard = [
+            ['Новый вопрос'],
+        ]
+        markup_key = ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+        )
+
+        update.message.reply_text(
+            text='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+            reply_markup=markup_key,
+        )
+        return States.CHOOSING
+    else:
+        reply_keyboard = [
+            ['Сдаться'],
+        ]
+        markup_key = ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+        )
+
+        update.message.reply_text(
+            text='Неправильно… Попробуешь ещё раз?',
+            reply_markup=markup_key,
+        )
+        return States.SCORE
+
+
+def handle_give_up(update, _, redis_db):
+    user = update.message.from_user
+    logger.info('%s: %s', user.first_name, update.message.text)
+    answer = re.sub(r"[\(\[].*?[\)\]]", "", redis_db.get(user.id)).strip()[:-1]
+    reply_keyboard = [
+        ['Новый вопрос'],
+    ]
+    markup_key = ReplyKeyboardMarkup(
+        reply_keyboard, one_time_keyboard=True, resize_keyboard=True
     )
 
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
-
-
-def main() -> None:
-    logging.basicConfig(
-        format='%(asctime)s - %(funcName)s -  %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
+    update.message.reply_text(
+        text=answer,
+        reply_markup=markup_key
     )
-    logger.setLevel(logging.DEBUG)
+    return States.CHOOSING
+
+
+def cancel(update, _):
+    user = update.message.from_user
+    logger.info('Пользователь %s отменил разговор.', user.first_name)
+    update.message.reply_text(
+        'Мое дело предложить - Ваше отказаться.\n'
+        'До встречи!',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+
+
+def main():
     env = Env()
     env.read_env()
+    try:
+        updater = Updater(env.str('TELEGRAM_BOT_TOKEN'))
+        redis_server = redis.StrictRedis(
+            host=env.str('REDIS_HOST'),
+            port=env.int('REDIS_PORT'),
+            password=env.str('REDIS_PASSWORD'),
+            decode_responses=True,
+            encoding='KOI8-R'
+        )
+        dispatcher = updater.dispatcher
 
-    logger.info('Бот запущен')
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('start', start)],
 
-    updater = Updater(token=env.str('TELEGRAM_BOT_TOKEN'), use_context=True)
-    dispatcher = updater.dispatcher
+            states={
+                States.CHOOSING: [
+                    MessageHandler(
+                        Filters.regex('^Новый вопрос$'),
+                        partial(handle_new_question_request, redis_db=redis_server),
+                    )
+                ],
+                States.SCORE: [
+                    MessageHandler(
+                        Filters.regex('^Сдаться$'),
+                        partial(handle_give_up, redis_db=redis_server),
+                    ),
+                    MessageHandler(
+                        Filters.text,
+                        partial(handle_solution_attempt, redis_db=redis_server),
+                    )
+                ],
+            },
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+            fallbacks=[CommandHandler('cancel', cancel)],
+        )
 
-    updater.start_polling()
-
-    updater.idle()
+        dispatcher.add_handler(conv_handler)
+        updater.start_polling()
+        updater.idle()
+    except Exception as err:
+        logging.error(err, exc_info=True)
 
 
 if __name__ == '__main__':
