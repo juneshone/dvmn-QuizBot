8232231a436e74950bb908081ea0e3cc40e60f35
juneshone <shamrova.julia@mail.ru>
2024-10-03 17:56:30 +0300

    Add content_file closure

--- collecting_content.py
+++ collecting_content.py
@@ -1,6 +1,6 @@
 import os 
 import re
-
+import json
 
 def get_content(content_folder):
     quiz_content = {}
@@ -8,11 +8,11 @@ def get_content(content_folder):
         file_path = os.path.join(content_folder, file)
         with open(file_path, 'r', encoding='KOI8-R') as content_file:
             text = content_file.read()
-            text_parts = text.split('\n\n')
-            for text_part in text_parts:
-                if 'Вопрос' in text_part:
-                    question = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
-                if 'Ответ' in text_part:
-                    answer = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
-                    quiz_content[question] = answer
+        text_parts = text.split('\n\n')
+        for text_part in text_parts:
+            if 'Вопрос' in text_part:
+                question = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
+            if 'Ответ' in text_part:
+                answer = re.split(':', text_part, maxsplit=1)[1].replace('\n', ' ')
+                quiz_content[question] = answer
     return quiz_content
